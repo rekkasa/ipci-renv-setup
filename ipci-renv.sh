771515218ed79b4d430d58aa66a09fc2bdc9baf8
rekkasa <a.rekkas@erasmusmc.nl>
2021-10-21 16:21:38 +0200

    install packages as you go

--- ipci-renv.sh
+++ ipci-renv.sh
@@ -6,22 +6,23 @@ echo "-------------------------"
 echo "---------------------------------"
 echo "## Installing required packages ##"
 echo "----------------------------------"
-sudo apt install wget build-essential gfortran  zlib1g zlib1g-dev \
- libbz2-dev liblzma-dev libpcre2-dev libcurl4-openssl-dev \
- default-jre default-jdk git libxml2-dev libssl-dev -y
+sudo apt install zip wget build-essential gfortran zlib1g\
+ zlib1g-dev libbz2-dev liblzma-dev libpcre2-dev\
+ libcurl4-openssl-dev default-jre default-jdk git\
+ libxml2-dev -y
 
 
 echo "------------------"
 echo "## Setting up R ##"
 echo "------------------"
-read -ep "R version: " rVersion
+cd && read -ep "R version: " rVersion
 majorRVersion=${rVersion:0:1}
 wget "https://cran.r-project.org/src/base/R-${majorRVersion}/R-${rVersion}.tar.gz"
 tar -xvf "R-${rVersion}.tar.gz"
 
 pushd "R-${rVersion}"
 ./configure --with-readline=no --with-x=no && make
-sudo ln -s ~/R-${rVersion}/bin/* /usr/bin
+sudo ln -s $HOME/R-${rVersion}/bin/* /usr/bin
 popd
 
 sudo R CMD javareconf
@@ -33,7 +34,13 @@ echo "------------------------------------"
 
 read -ep "Github location of project: " githubStudy
 git clone https://github.com/${githubStudy}.git
+projectDir=$HOME/${githubStudy##*/}
 
-pushd ${githubStudy##*/}
-../renv-setup.R
+until $HOME/ipci-renv-setup/renv-setup.R $projectDir; do
+	read -a packages
+	sudo apt-get -y install ${packages[@]}
+done
+pushd $projectDir
+zip -rv renv.zip renv
+mv renv.zip $HOME
 popd
--- renv-setup.R
+++ renv-setup.R
@@ -2,5 +2,10 @@
 
 options(repos=structure(c(CRAN="http://cran.r-project.org")))
 
+args <- commandArgs(trailingOnly = TRUE)
+setwd(args[1])
+
 install.packages("renv")
+renv::activate()
 renv::restore()
+renv::isolate()
